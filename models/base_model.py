4e2443797954b10e0dcc3f044059b5c759b98bc8
ritakendi <ritabetty77@gmail.com>
2022-08-08 16:32:28 +0300

    added an argument to setattr

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
                         self.updated_at = datetime.strptime(
                             value, "%Y-%m-%dT%H:%M:%S.%f")
                     else:
-                        setattr(key, value)
+                        setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()

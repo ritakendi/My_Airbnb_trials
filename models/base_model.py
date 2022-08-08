3f141f3ca39a58d123f63c762213f8eb39b939e9
ritakendi <ritabetty77@gmail.com>
2022-08-08 21:31:47 +0300

    added an __init to models modified

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """This module defines a Baseclass for all models in our Airbnb clone"""
 from datetime import datetime
 import uuid
+from models import storage
 
 
 class BaseModel:
@@ -24,6 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new()
 
     def __str__(self):
         """string representation of the basemodel"""
@@ -34,6 +36,7 @@ class BaseModel:
             with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values
---
+++ models/engine/file_storage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Contains File storage file"""
+import json
+
+
+class FileStorage:
+    """Serializes instances to a JSON file
+        and desserializes JSON files to intances
+    """
+    # string - path to the JSON fie
+    __file_path = "file.json"
+    # dictionary - empty but will store all objects by <class name>.id
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        with open(self.__file_path, "w") as f:
+            json.dump(self.__objects, f)
+    
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, 'r') as f:
+               self.__objects = json.load(f)
+        except:
+            pass

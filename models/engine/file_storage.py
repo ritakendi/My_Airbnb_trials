5585f3af581394e16aa51686eea8e3dc049754ca
ritakendi <ritabetty77@gmail.com>
2022-08-09 09:31:21 +0300

    my trials

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-"""Contains FileStorage class"""
-
+"""Contains File storage file"""
 import json
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
-    """serializes instances to a JSON file & deserializes back to instances"""
-
-    # string - path to the JSON file
+    """Serializes instances to a JSON file
+        and desserializes JSON files to intances
+    """
+    # string - path to the JSON fie
     __file_path = "file.json"
     # dictionary - empty but will store all objects by <class name>.id
     __objects = {}
@@ -28,24 +18,18 @@ class FileStorage:
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        if obj is not None:
-            key = obj.__class__.__name__ + "." + obj.id
-            self.__objects[key] = obj
+        key = obj.__class__.__name__ + "." + obj["id"]
+        self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
-        json_objects = {}
-        for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict()
-        with open(self.__file_path, 'w') as f:
-            json.dump(json_objects, f)
+        """serializes __objects to the JSON file"""
+        with open(self.__file_path, "w") as f:
+            json.dump(self.__objects, f)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, 'r') as f:
-                jo = json.load(f)
-            for key in jo:
-                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except Exception:
+                self.__objects = json.load(f)
+        except:
             pass
